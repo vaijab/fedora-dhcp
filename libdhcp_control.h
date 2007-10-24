dd43804e32d146058020be6803828c6125f8d594
David Cantrell <dcantrel@fedoraproject.org>
2007-10-24 19:22:41 +0000

    - Install libdhcp_control.h to /usr/include/isc-dhcp/libdhcp_control.h - Update libdhcp4client patch to use new libdhcp_control.h location - Remove __fedora_contrib/ subdirectory in /usr/share/doc/dhcp-3.1.0,     install those docs to /usr/share/doc/dhcp-3.1.0

--- libdhcp_control.h
+++ libdhcp_control.h
@@ -1,24 +1,30 @@
 /* libdhcp_control.h
  *
- *  DHCP client control API for libdhcp, a minimal interface to the
- *  ISC dhcp IPv4 client libdhcp4client library,
- *  and to the dhcpv6 DHCPv6 client libdhcp6client library.
+ * DHCP client control API for libdhcp, a minimal interface to the
+ * ISC dhcp IPv4 client libdhcp4client library,
+ * and to the dhcpv6 DHCPv6 client libdhcp6client library.
  *
- *  Each DHCP client library must include this file to be controlled
- *  by libdhcp.
+ * Each DHCP client library must include this file to be controlled
+ * by libdhcp.
  *
- *  Copyright(C) Jason Vas Dias <jvdias@redhat.com> Red Hat Inc. May 2006
+ * Copyright (C) 2006  Red Hat, Inc. All rights reserved.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation at 
- *           http://www.fsf.org/licensing/licenses/gpl.txt
- *  and included in this software distribution as the "LICENSE" file.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions of
+ * the GNU General Public License v.2, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY expressed or implied, including the implied warranties of
+ * MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.  You should have received a copy of the
+ * GNU General Public License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+ * source code or documentation are not subject to the GNU General Public
+ * License and may only be used or replicated with the express permission of
+ * Red Hat, Inc.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Red Hat Author(s): Jason Vas Dias
+ *                    David Cantrell
  */
 #ifndef LIBDHCP_CONTROL_H
 #define LIBDHCP_CONTROL_H
